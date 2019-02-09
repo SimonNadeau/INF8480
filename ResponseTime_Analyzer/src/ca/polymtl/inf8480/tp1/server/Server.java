b86c3f507351fc8508db1b05b436df9dbdb7a9de
Mathieu <mathieu.chateauvert@hotmail.com>
2019-02-09 18:58:01 -0500

    Finalisation avant test

--- ResponseTime_Analyzer/src/ca/polymtl/inf8480/tp1/server/Server.java
+++ ResponseTime_Analyzer/src/ca/polymtl/inf8480/tp1/server/Server.java
@@ -7,6 +7,9 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.bind.DatatypeConverter;
 
@@ -16,14 +19,19 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.attribute.UserDefinedFileAttributeView;
 import java.io.BufferedReader;
 
 
 import ca.polymtl.inf8480.tp1.shared.ServerInterface;
 
 public class Server implements ServerInterface {
+
+    private int numberOfClients;
 
 	public static void main(String[] args) {
 		Server server = new Server();
@@ -33,12 +41,14 @@ public class Server implements ServerInterface {
 	public Server() {
         super();
 
+        numberOfClients = 0;
+        createInbox();
         try {
             String fileName = ServerInterface.userPath.concat("Server/lock.txt");
             File file = new File(fileName);
 
             FileWriter f2 = new FileWriter(file, false);
-            f2.write("false");
+            f2.write("0");
             f2.close();
 
         } catch (FileNotFoundException e) {
@@ -69,16 +79,7 @@ public class Server implements ServerInterface {
 		}
 	}
 
-	/*
-	 * Méthode accessible par RMI. Ne fait rien, mais prend un tableau de taille variable en argument.
-	 */
-	@Override
-	public void execute() throws RemoteException {
-    }
-
-    @Override
-    public boolean openSession(String login, String password) throws RemoteException {
-        boolean bool_login = false;
+    private void createInbox() {
 
         try {
             String fileName = ServerInterface.userPath.concat("Server/user.txt");
@@ -90,9 +91,37 @@ public class Server implements ServerInterface {
             while((line = br.readLine()) != null){
                 //process the line
                 String[] parts = line.split(" ");
+                new File(ServerInterface.userPath.concat("Server/").concat(parts[0])).mkdirs();
+            }
+
+            br.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Erreur: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Erreur: " + e.getMessage());
+        }
+
+    }
+
+
+    @Override
+    public int openSession(String login, String password) throws RemoteException {
+        int clientId = 0;
+        try {
+            String fileName = ServerInterface.userPath.concat("Server/user.txt");
+            File file = new File(fileName);
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+
+            while((line = br.readLine()) != null){
+                //process the line
+                String[] parts = line.split(" ");
                 if (login.equals(parts[0]) && password.equals(parts[1])){
-                    bool_login = true;
-                    System.out.println("Connexion de l'utilisateur " + login);
+                    numberOfClients += 1;
+                    clientId = numberOfClients;
+                    System.out.println("Connexion de l'utilisateur " + login + " avec ID " + String.valueOf(clientId));
                 }
             }
 
@@ -104,11 +133,11 @@ public class Server implements ServerInterface {
             System.out.println("Erreur: " + e.getMessage());
         }
 
-        return bool_login;
+        return clientId;
     }
 
     @Override
-    public boolean lockGroupList() throws RemoteException {
+    public boolean lockGroupList(int clientId) throws RemoteException {
         boolean locked = false;
 
         try {
@@ -116,14 +145,14 @@ public class Server implements ServerInterface {
             File file = new File(fileName);
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
+            String line = br.readLine();
 
-            if (br.readLine().equals("false")) {
+            if (line.equals("0") || line.equals(String.valueOf(clientId))) {
                 locked = true;
                 FileWriter f2 = new FileWriter(file, false);
-                f2.write("true");
+                f2.write(String.valueOf(clientId));
                 f2.close();
-
-            }
+            } 
 
             br.close();
 
@@ -137,13 +166,13 @@ public class Server implements ServerInterface {
     }
 
     @Override
-    public void pushGroupList() throws RemoteException {
+    public void pushLocalGroupList(File localFile) throws RemoteException {
         try {
-            String fileName = ServerInterface.userPath.concat("Server/lock.txt");
-            File file = new File(fileName);
+            String lockFileName = ServerInterface.userPath.concat("Server/lock.txt");
+            File lockFile = new File(lockFileName);
 
-            FileWriter f2 = new FileWriter(file, false);
-            f2.write("false");
+            FileWriter f2 = new FileWriter(lockFile, false);
+            f2.write("0");
             f2.close();
 
         } catch (FileNotFoundException e) {
@@ -152,7 +181,12 @@ public class Server implements ServerInterface {
             System.out.println("Erreur: " + e.getMessage());
         }
 
-        System.out.println("Liste de groupe a été publié.");
+        File fileToDelete = new File(ServerInterface.userPath.concat("Server/groupServer.txt"));
+        fileToDelete.delete();
+
+        System.out.println("Mise à jour de la liste des groupes serveur");
+
+        localFile.renameTo(fileToDelete);  
     }
 
     @Override
@@ -187,11 +221,267 @@ public class Server implements ServerInterface {
         return new File(ServerInterface.userPath.concat("Server/groupServer1.txt"));
     }
 
+    @Override
+    public boolean sendMail(String subject, String addrDest, String content, String clientAdress) throws RemoteException {
+        
+        ArrayList<ArrayList<String>> groupList = new ArrayList<ArrayList<String>>();
+        try {
+            File file = new File(ServerInterface.userPath.concat("Server/groupServer.txt"));
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ");
+                ArrayList<String> tempList = new ArrayList<String>();
+                for (String part: parts){
+                    // Ajout au tableau des groupes
+                    tempList.add(part);
+                }
+                groupList.add(tempList);
+            }
+            br.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Erreur: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Erreur: " + e.getMessage());
+        }
+        for (ArrayList<String> group : groupList){
+            if (group.get(0).equals(addrDest)){
+                boolean emailSent = true;
+                for (String user: group){
+                    if (!user.equals(addrDest)){
+                        emailSent = emailSent && sendToUser(subject, user, content, clientAdress);
+                    }
+                }
+                return emailSent;
+            }
+        }
+        return sendToUser(subject, addrDest, content, clientAdress);
+    }
+
+    private boolean sendToUser(String subject, String addrDest, String content, String clientAdress){
+
+        ArrayList<String> userList = new ArrayList<String>();
+        try {
+            File file = new File(ServerInterface.userPath.concat("Server/user.txt"));
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ");
+                userList.add(parts[0]);
+            }
+            br.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Erreur: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Erreur: " + e.getMessage());
+        }
+
+        for (String user : userList){
+            if (user.equals(addrDest)){
+                try {
+                    String fileName = ServerInterface.userPath + "Server/" + addrDest + "/" + subject + ".txt";
+                    File file = new File(fileName);
+                        
+                    FileWriter f2 = new FileWriter(file, true);
+                    f2.write("unread\n");
+                    f2.write(clientAdress + "\n");
+                    f2.write(content);
+                    f2.close();
+        
+                } catch (FileNotFoundException e) {
+                    System.out.println("Erreur: " + e.getMessage());
+                } catch (IOException e) {
+                    System.out.println("Erreur: " + e.getMessage());
+                }
+                return true;
+            }
+        }
+        return false;
+    }
 
     @Override
-    public String send() throws RemoteException {
-        return "Only one at a time";
+    public String listMails(boolean justUnread, String clientAdress) throws RemoteException {
+
+        String inboxList = "";
+        File folder = new File(ServerInterface.userPath + "Server/" + clientAdress);
+        int numberOfFile = 0;
+        int numberOfUnreadFile = 0;
+
+
+        for (File fileEntry : folder.listFiles()) {
+            String fileName = fileEntry.getName();
+            String subject = fileName.split(".txt")[0];
+            String status = "";
+            String sender = "";
+
+            try {
+                FileReader fr = new FileReader(fileEntry);
+                BufferedReader br = new BufferedReader(fr);
+                status = br.readLine();
+                sender = br.readLine();
+                br.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("Erreur: " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("Erreur: " + e.getMessage());
+            }
+
+            SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM d HH:mm");
+            String dateTime = sdf.format(fileEntry.lastModified());
+
+            if (status.equals("unread")){
+                status = "N";
+                numberOfUnreadFile += 1;
+            } else {
+                status = "-";
+            }
+            numberOfFile += 1;
+
+            if (justUnread){
+                if (status.equals("N")){
+                    inboxList += (String.valueOf(numberOfUnreadFile) + " " + status + "   " + sender + "  " + dateTime + "    " + subject + "\n");
+                }
+            } else {
+                inboxList += (String.valueOf(numberOfFile) + "  " + status + "   " + sender + "  " + dateTime + "    " + subject + "\n");
+            }
+        }
+        String returnMessage = "";
+        if (justUnread){
+            returnMessage = String.valueOf(numberOfUnreadFile) + " courriers sont non-lus\n";
+        } else {
+            returnMessage = String.valueOf(numberOfFile) + " courriers dont " + String.valueOf(numberOfUnreadFile) + " sont non-lus\n";
+        }
+
+        returnMessage += inboxList;
+
+        return returnMessage;
     }
+
+    @Override
+    public boolean deleteMail(int mailNumber, String clientAdress) throws RemoteException {
+
+        boolean deleted = false;
+        int fileNumber = 0;
+        File folder = new File(ServerInterface.userPath + "Server/" + clientAdress);
+
+        for (File fileEntry : folder.listFiles()) {
+
+            fileNumber += 1;
+
+            if (fileNumber == mailNumber) {
+                if (fileEntry.delete()){
+                    deleted = true;
+                }
+            }
+        }
+
+        return deleted;
+    }
+
+    @Override
+    public String readMail(int mailNumber, String clientAdress) throws RemoteException {
+        String emailContent = "";
+        String readEmail = "read\n";
+
+        int fileNumber = 0;
+        File folder = new File(ServerInterface.userPath + "Server/" + clientAdress);
+
+        for (File fileEntry : folder.listFiles()) {
+
+            fileNumber += 1;
+
+            if (fileNumber == mailNumber) {
+                try {
+                    FileReader fr = new FileReader(fileEntry);
+                    BufferedReader br = new BufferedReader(fr);
+                    
+                    br.readLine();
+                    readEmail += br.readLine() + "\n";
+                    String line;
+        
+                    while((line = br.readLine()) != null){
+                        emailContent += line + "\n";
+                    }
+                    readEmail += emailContent;
+                    br.close();
+
+                    fileEntry.delete();
+                    FileWriter f2 = new FileWriter(fileEntry, false);
+                    f2.write(readEmail);
+                    f2.close();
+
+                } catch (FileNotFoundException e) {
+                    System.out.println("Erreur: " + e.getMessage());
+                } catch (IOException e) {
+                    System.out.println("Erreur: " + e.getMessage());
+                }
+            }
+        }
+        return emailContent;
+    }
+
+    @Override
+    public String searchMail(String keywords, String clientAdress) throws RemoteException {
+
+        File folder = new File(ServerInterface.userPath + "Server/" + clientAdress);
+        int mailNumber = 0;
+        String inboxString = "";
+        int numberOfFile = 0;
+
+        for (File fileEntry : folder.listFiles() ) {
+            mailNumber += 1;
+            boolean added = false;
+
+            for (String keyword : keywords.split(" ")) {
+                if (readMail(mailNumber, clientAdress).contains(keyword) && !added){
+                    String fileName = fileEntry.getName();
+                    String subject = fileName.split(".txt")[0];
+                    String status = "";
+                    String sender = "";
+        
+                    try {
+                        FileReader fr = new FileReader(fileEntry);
+                        BufferedReader br = new BufferedReader(fr);
+                        status = br.readLine();
+                        sender = br.readLine();
+                        br.close();
+                    } catch (FileNotFoundException e) {
+                        System.out.println("Erreur: " + e.getMessage());
+                    } catch (IOException e) {
+                        System.out.println("Erreur: " + e.getMessage());
+                    }
+        
+                    SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM d HH:mm");
+                    String dateTime = sdf.format(fileEntry.lastModified());
+        
+                    if (status.equals("unread")){
+                        status = "N";
+                    } else {
+                        status = "-";
+                    }
+                    numberOfFile += 1;
+
+                    inboxString += (String.valueOf(mailNumber) + "    " + status + "   " + sender + "  " + dateTime + "    " + subject + "\n");
+                    added = true;
+                }
+            }
+        }
+
+        String returnMessage = String.valueOf(numberOfFile) + " courriers qui correspondent à votre recherche sont trouvés.\n";
+        returnMessage += inboxString;
+
+        return returnMessage;
+    }
+
+
+
+
 
 
 }
--- ResponseTime_Analyzer/src/ca/polymtl/inf8480/tp1/shared/ServerInterface.java
+++ ResponseTime_Analyzer/src/ca/polymtl/inf8480/tp1/shared/ServerInterface.java
@@ -8,11 +8,14 @@ public interface ServerInterface extends Remote {
 
     String userPath = "/Users/mathieuchateauvert/Documents/École/Polytechnique/Session6/INF8480/INF8480/ResponseTime_Analyzer/src/ca/polymtl/inf8480/tp1/";
 
-    void execute() throws RemoteException;
-    boolean openSession(String login, String password) throws RemoteException;
-    String send() throws RemoteException;
-    boolean lockGroupList() throws RemoteException;
-    void pushGroupList() throws RemoteException;
+    int openSession(String login, String password) throws RemoteException;
+    boolean lockGroupList(int clientId) throws RemoteException;
     boolean getGroupList(String clientChecksum) throws RemoteException;
     File getServerGroupList() throws RemoteException;
+    void pushLocalGroupList(File file) throws RemoteException;
+    boolean sendMail(String subject, String addrDest, String content, String clientAdress) throws RemoteException;
+    String listMails(boolean justUnread, String clientAdress)throws RemoteException;
+    boolean deleteMail(int mailNumber, String clientAdress) throws RemoteException;
+    String readMail(int mailNumber, String clientAdress) throws RemoteException;
+    String searchMail(String keywords, String clientAdress)throws RemoteException;
 }
